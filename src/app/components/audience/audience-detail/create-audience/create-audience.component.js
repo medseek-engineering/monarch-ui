e8bb15755e08361c33b2e194b23e18ae93a86c23
Morgan Stone <morgan.stone@medseek.com>
2017-02-24 16:40:18 -0600

    create audience page changes

--- src/app/components/audience/audience-detail/create-audience/create-audience.component.js
+++ src/app/components/audience/audience-detail/create-audience/create-audience.component.js
@@ -6,34 +6,41 @@ export const CreateAudienceComponent = {
   },
   template: `
 
-    <div class="row row-flush page-header">
-      <div class="columns medium-2">
-        <small>Healthcare Marketing<br>Peachtree Hospital</small>
+    <div class="full-screen">
+      <header class="full-screen-header">
+        <a ui-sref="dashboard" class="full-screen-logo"></a>
+        <button
+          class="close-button"
+          aria-label="Close"
+          type="button"
+          ng-click="$ctrl.onCancel($ctrl.selectedAudience)">
+           <span aria-hidden="true">&times;</span>
+        </button>
+        <h3 class="full-screen-heading">Create List: Create Audience</h3>
+        <small class="full-screen-small-text">Healthcare Marketing - Peachtree Hospital</small>
+      </header>
+      <div class="row row-flush page-header">
+        
+        <div class="column medium-4">
+
+          <h2>{{$ctrl.selectedAudience.title}}</h2>
+        </div>
+        <div class="column medium-6 clearfix">
+          <ul
+            class="menu float-right"
+            style="padding-top: 0.5rem">
+            <li>
+              <a
+                href=""
+                ng-click="$ctrl.save($ctrl.selectedAudience)"
+                class="button primary">Save Audience</a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div class="column medium-4">
-        <h2>Create Audience</h2>
-      </div>
-      <div class="column medium-6 clearfix">
-        <ul
-          class="menu float-right"
-          style="padding-top: 0.5rem">
-          <li style="padding-right: 1rem">
-            <a
-              href=""
-              ng-click="$ctrl.onCancel($ctrl.selectedAudience)"
-              class="button">Cancel</a>
-          </li>
-          <li>
-            <a
-              href=""
-              ng-click="$ctrl.save($ctrl.selectedAudience)"
-              class="button primary">Save Audience</a>
-          </li>
-        </ul>
-      </div>
+      <audience-builder
+        person-list="$ctrl.selectedAudience.persons"></audience-builder>
     </div>
-    <audience-builder
-      person-list="$ctrl.selectedAudience.persons"></audience-builder>
   `,
   controller: class CreateAudienceComponent {
     constructor(audiences) {
